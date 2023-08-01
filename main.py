4fde8f7bc696cbf6f198889295180eea118f4eac
cyhanyue <46631400+cyhanyue@users.noreply.github.com>
2023-08-01 10:31:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
 template = """
-    Below is an email that may be poorly worded.
+    Below are paragraphs that contain incorrect grammar.
     Your goal is to:
-    - Properly format the email
+    - Correct the grammar of the paragraphs
     - Convert the input text to a specified tone
     - Convert the input text to a specified dialect
 
     Here are some examples different Tones:
-    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
-    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+    - Formal: It was great to meet you. 
+    - Warm: It was wonderful to meet you!  
+    - Casual: Great to meet you!
 
     Here are some examples of words in different dialects:
     - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
@@ -21,18 +22,16 @@ template = """
     - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
     - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
 
-    Please start the email with a warm introduction. Add the introduction if you need to.
-    
-    Below is the email, tone, and dialect:
+    Below is the text, tone, and dialect:
     TONE: {tone}
     DIALECT: {dialect}
-    EMAIL: {email}
+    PARAGRAPHS: {text}
     
     YOUR {dialect} RESPONSE:
 """
 
 prompt = PromptTemplate(
-    input_variables=["tone", "dialect", "email"],
+    input_variables=["tone", "dialect", "text"],
     template=template,
 )
 
@@ -42,18 +41,18 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.set_page_config(page_title="Grammar Genie", page_icon=":robot:")
+st.header("Grammar Genie")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("Write Better Emails!")
+    st.markdown("Beautify your writing")
 
 with col2:
     pass
 
-st.markdown("## Enter Your Email To Convert")
+st.markdown("## Enter Your Paragraphs To Beautify")
 
 def get_api_key():
     input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
@@ -64,8 +63,8 @@ openai_api_key = get_api_key()
 col1, col2 = st.columns(2)
 with col1:
     option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
+        'Which tone would you like your paragraphs to have?',
+        ('Formal', 'Warm', 'Casual'))
     
 with col2:
     option_dialect = st.selectbox(
@@ -73,32 +72,32 @@ with col2:
         ('American', 'British'))
 
 def get_text():
-    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    input_text = st.text_area(label="Text Input", label_visibility='collapsed', placeholder="Your Email...", key="text_input")
     return input_text
 
-email_input = get_text()
+text_input = get_text()
 
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email. The maximum length is 700 words.")
-    st.stop()
+# if len(text_input.split(" ")) > 700:
+#     st.write("Please enter a shorter email. The maximum length is 700 words.")
+#     st.stop()
 
 def update_text_with_example():
     print ("in updated")
-    st.session_state.email_input = email_input
+    st.session_state.text_input = text_input
 
-st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+st.button("*See An Example*", type='secondary', help="Click to see an example of the text you will be converting.", on_click=update_text_with_example)
 
-st.markdown("### Your Converted Email:")
+st.markdown("### Your Beautified Text:")
 
-if email_input:
+if text_input:
     if not openai_api_key:
         st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
         st.stop()
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    prompt_with_text = prompt.format(tone=option_tone, dialect=option_dialect, email=text_input)
 
-    formatted_email = llm(prompt_with_email)
+    formatted_text = llm(prompt_with_text)
 
-    st.write(formatted_email)
+    st.write(formatted_text)
